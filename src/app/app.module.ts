d84d7dc6a4f4fa93f9cd78fa3696cb177c8973d7
MaikolCode <maikol320434@gmail.com>
2021-10-19 13:06:43 -0500

    Carga inicial - add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { GetUserComponent } from './components/get-user/get-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    EditUserComponent,
+    GetUserComponent
   ],
   imports: [
     BrowserModule,
